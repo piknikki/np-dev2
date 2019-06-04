1430d4e1e2f88dbb2fd589e35ec723b446c277ed
Nikki Petersen <piknik@me.com>
2019-06-04 10:27:30 -0600

    Updates

---
+++ precache-manifest.59d40024a89cee2987116641d165efc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4887c27fac75bea9fca213543d4eb565",
+    "url": "/np-dev2/index.html"
+  },
+  {
+    "revision": "42fa40a24ee44dd53e6b",
+    "url": "/np-dev2/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cd34c88f2f0b8eaad3cf",
+    "url": "/np-dev2/static/css/main.25a59140.chunk.css"
+  },
+  {
+    "revision": "42fa40a24ee44dd53e6b",
+    "url": "/np-dev2/static/js/2.5aad20b0.chunk.js"
+  },
+  {
+    "revision": "cd34c88f2f0b8eaad3cf",
+    "url": "/np-dev2/static/js/main.c2d84473.chunk.js"
+  },
+  {
+    "revision": "97326332d9cf2263621b",
+    "url": "/np-dev2/static/js/runtime~main.f1c19f3e.js"
+  }
+]);
